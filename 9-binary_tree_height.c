842ee6050b7059f21e3275f8f46cb4b6a7bcfa5f
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-03-28 13:30:52 -0700

    Task 9 - fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		left_h = 1 + binary_tree_height(tree->left);
 
 	if (tree->right)
-		right_h = 1 + binary_tree_height(tree->left);
+		right_h = 1 + binary_tree_height(tree->right);
 
 	return (left_h > right_h ? left_h : right_h);
 }
