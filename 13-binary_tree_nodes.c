3b44af2418f1483dac2b42c0f137ad6f6cb86492
Soufiane Sadgali <madskillz.oneofone@gmail.com>
2024-03-28 13:24:25 -0700

    task 13 - checking

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -3,7 +3,8 @@
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  * @tree: pointer to the root node of a tree.
  *
- * Return: total number of internal nodes, otherwise 0 if tree is empty
+ * Return:
+ * total number of internal nodes, otherwise 0 if tree is empty
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
